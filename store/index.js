0a83005f3f3ba99a6986b2a8f4b4202e3ec0cd89
Tess Monical <tess.monical@gmail.com>
2018-02-21 13:47:55 -0600

    adds current tab to the store

--- store/index.js
+++ store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import books from './books';
+import tabs from './tabs';
 
 const reducer = combineReducers({
   books,
+  tabs,
 });
 const middleware = composeWithDevTools(applyMiddleware(
   createLogger({ collapsed: true })
@@ -13,4 +15,5 @@ const store = createStore(reducer, middleware);
 
 export default store;
 export * from './books';
+export * from './tabs';
 
---
+++ store/tabs.js
@@ -0,0 +1,23 @@
+// action types
+const SET_TAB = 'SET_TAB';
+
+// default state
+const defaultTab = 0;
+
+// action creators
+const setTab = (tabNumber) => ({
+  type: SET_TAB,
+  tabNumber,
+});
+
+// thunks would go here if needed
+
+// reducer
+export default function (state = defaultTab, action) {
+  switch (action.type) {
+    case SET_TAB:
+      return action.tabNumber;
+    default:
+      return state;
+  }
+}
