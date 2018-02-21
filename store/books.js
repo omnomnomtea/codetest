6f78cd8ed7ce2bb63dfb72a34a610116cae3b31d
Tess Monical <tess.monical@gmail.com>
2018-02-21 13:34:22 -0600

    books reducer

---
+++ store/books.js
@@ -0,0 +1,31 @@
+import booksToLoad from 'data.json';
+
+// action types
+const LOAD_BOOKS = 'LOAD_BOOKS';
+
+// default state
+const defaultBooks = [];
+
+// action creators
+const loadBooks = (books) => ({
+  type: LOAD_BOOKS,
+  books,
+});
+
+// thunks
+// this is a fake thunk. We could just have set the initial state to booksToLoad
+// I'm sort of mocking what I would do if this were an axios request,
+// since that's a more realistic use case than loading data from json
+export const fetchBooks = () =>
+  dispatch =>
+    dispatch(loadBooks(booksToLoad));
+
+// reducer
+export default function (state = defaultBooks, action) {
+  switch (action.type) {
+    case LOAD_BOOKS:
+      return action.books;
+    default:
+      return state;
+  }
+}
